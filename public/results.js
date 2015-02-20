6881bb843ac72906bc2da74fd51e07d7c651ef28
Cfo613 <cforan13@gmail.com>
2015-02-19 20:48:10 -0500

    updated weakness section on results page

--- public/results.js
+++ public/results.js
@@ -5,4 +5,25 @@ window.addEventListener('load', function() {
     calculate.id = 'hide';
     results.id = 'show';
   }, 6000);
-})
+});
+
+// setTimeout(function() { weaknessPopover()}, 7000);
+//
+// var weaknessPopover = function() {
+//   var weaknesses = [];
+//   weaknesses = document.querySelectorAll(".weakness");
+//   console.log(weaknesses);
+//   console.log(weaknesses[0].id);
+//   for (var i = 0; i < weaknesses.length; i++) {
+//     console.log(weaknesses[i].id);
+//     if (weaknesses[i].id === "Bitter") {
+//       console.log("made it here");
+//       $(weaknesses[i]).popover({
+//         "data-container": "body",
+//         "data-content": "As a superhero, bitter taste perception is critical, as all heroes harness the bitter taste of evil in order to achieve their potential.",
+//         "data-placement": "top",
+//         "data-trigger": "click"
+//       });
+//     }
+//   }
+// }
