07ea45eb2d2ebd73f9a8580d985b7e1bc45689a4
Cfo613 <cforan13@gmail.com>
2015-02-23 17:10:53 -0500

    updated js to include popover functionality for weaknesses

--- public/results.js
+++ public/results.js
@@ -4,21 +4,44 @@ window.addEventListener('load', function() {
   setTimeout(function() {
     calculate.id = 'hide';
     results.id = 'show';
-  }, 6000);
+  }, 4000);
 });
 
-// setTimeout(function() { weaknessPopover()}, 7000);
-//   var weaknessPopover = function() {
-//   var weaknesses = [];
-//   weaknesses = document.querySelectorAll(".weakness");
-//   console.log(weaknesses);
-//   console.log(weaknesses[0].id);
-//   for (var i = 0; i < weaknesses.length; i++) {
-//     console.log(weaknesses[i].id);
-//     if (weaknesses[i].id === "Bitter") {
-//       console.log("made it here");
-//       (weaknesses[i]).popover("data-container"="body","data-toggle"="popover", "data-content"="As a superhero, bitter taste perception is critical, as all heroes harness the bitter taste of evil in order to achieve their potential.", "data-placement"="top","data-trigger"="click");
-//       })
-//     }
-//   }
-// }
+setTimeout(function() { weaknessPopover()}, 5000);
+
+var weaknessPopover = function() {
+  var weaknesses = [];
+  weaknesses = document.querySelectorAll(".weakness");
+  for (var i = 0; i < weaknesses.length; i++) {
+    if (weaknesses[i].id === "Alcohol") {
+        $(weaknesses[i]).popover({
+          content: "When hanging with your superhero friends, alcohol flush is not a good look - just sayin."
+        })
+    }
+    if (weaknesses[i].id === "Bitter") {
+        $(weaknesses[i]).popover({
+          content: "As a superhero, bitter taste perception is critical, as all heroes harness the bitter taste of evil in order to achieve their potential."
+        })
+    }
+    if (weaknesses[i].id === "Lactose") {
+        $(weaknesses[i]).popover({
+          content: "Superheroes need to be able to tolerate whatever is thrown at them - never good to have lactose as your kryptonite."
+        })
+    }
+    if (weaknesses[i].id === "Male") {
+        $(weaknesses[i]).popover({
+          content: "While looks aren't everything, not many people want a balding superhero coming to their rescue - sorry but the truth hurts!"
+        })
+    }
+    if (weaknesses[i].id === "Red") {
+        $(weaknesses[i]).popover({
+          content: 'This disease is called "Gingervitis". Kids who have Gingervitis cannot be cured.'
+        })
+    }
+    if (weaknesses[i].id === "Smoking") {
+        $(weaknesses[i]).popover({
+          content: "As a role model, a true superhero would want to set the right example and not smoke. Also, those lungs won't be super forever! Or will they?"
+        })
+    }
+  }
+};
